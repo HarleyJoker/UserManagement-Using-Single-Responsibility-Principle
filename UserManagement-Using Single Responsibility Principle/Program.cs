542699004625fcc3ee334ecda4ebea7d34e257c5
Nosipho Mbuyazi <nmbuyazi@hollywoodbets.net>
2024-05-13 20:34:40 +0200

    added a UserManagers.cs class that conforms to the single responsibility principle

--- UserManagement-Using Single Responsibility Principle/Program.cs
+++ UserManagement-Using Single Responsibility Principle/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using UserManagement_Using_Single_Responsibility_Principle;
+using static UserManagement_Using_Single_Responsibility_Principle.UserManagers;
+
+namespace UserManagement_Using_Single_Responsibility_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UserManager userManager = new UserManager();
+            //// Add users
+            //userManager.AddUser("Alice");
+            //userManager.AddUser("Bob");
+
+            //// Keep the console window open
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+
+
+            // Create an instance of NotificationService
+            NotificationService notificationService = new NotificationService();
+
+            // Pass the NotificationService instance to UserManager
+            UserManagers userManager = new UserManagers(notificationService);
+
+            // Add users
+            userManager.AddUser("Alice");
+            userManager.AddUser("Bob");
+
+            // Remove a user
+            userManager.RemoveUser("Alice");
+
+            // Keep the console window open
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ UserManagement-Using Single Responsibility Principle/UserManagers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement_Using_Single_Responsibility_Principle
+{
+    public class UserManagers
+    {
+        public List<string> Users { get; set; }
+        private NotificationService _notificationService;
+
+        public UserManagers(NotificationService notificationService)
+        {
+            Users = new List<string>();
+            _notificationService = notificationService;
+        }
+
+        public void AddUser(string user)
+        {
+            Users.Add(user);
+            _notificationService.SendEmail(user, "You have been added.");
+        }
+
+        public void RemoveUser(string user)
+        {
+            Users.Remove(user);
+            _notificationService.SendEmail(user, "You have been removed.");
+        }
+
+        public class NotificationService
+        {
+            public void SendEmail(string user, string message)
+            {
+                // Simulate sending an email
+                Console.WriteLine($"Email to {user}: {message}");
+            }
+
+        }
+    }
+}
